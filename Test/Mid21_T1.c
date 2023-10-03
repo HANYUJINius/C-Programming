741a4bcd5e1f5e5380d0f0caf350b2c950c3d476
Han Yujin <133995050+HANYUJINius@users.noreply.github.com>
2023-10-03 23:18:16 +0900

    2021년 컴프 중간고사 1번

---
+++ Test/Mid21_T1.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void initArray(int arr[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		scanf("%d", &arr[i]);
+}
+
+void printArray(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+		printf("%d ", arr[i]);
+}
+
+void turnArray(int array1[], int array2[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if(i == size -1)
+			array2[i] = array1[0];
+		else
+			array2[i] = array1[i + 1];
+	}
+}
+
+int main(void)
+{
+	int arr1[5];
+	int arr2[5];
+
+	initArray(arr1, 5);
+	turnArray(arr1, arr2, 5);
+	printArray(arr2, 5);
+
+	return 0;
+}
