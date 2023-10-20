6938066340bd4f18a42913954836dd2bb6c311e2
Han Yujin <133995050+HANYUJINius@users.noreply.github.com>
2023-10-20 12:11:19 +0900

    Update Mid21_T1.c/2021년 컴프 중간고사 1번

--- Test/Mid21_T1.c
+++ Test/Mid21_T1.c
@@ -16,12 +16,13 @@ void printArray(int arr[], int size)
 
 void turnArray(int array1[], int array2[], int size)
 {
-	for (int i = 0; i < size; i++)
+	int i;
+	for (i = 0; i < size; i++)
 	{
-		if(i == size -1)
-			array2[i] = array1[0];
+		if (i == 0)
+			array2[i] = array1[size - 1];
 		else
-			array2[i] = array1[i + 1];
+			array2[i] = array1[i - 1];
 	}
 }
 
